caf3a6161ab976f7ccc5126e2ef0766da0ad90f6
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-07-11 22:37:31 +0200

    First version of pcheck.ml.  The semantics of proof need to be revised though.

--- elpi/pcheck.ml
+++ elpi/pcheck.ml
@@ -1,5 +1,5 @@
 (* ------------------------------------------------------------------------- *)
-(* TODO: I the first argument of "Pimp_r of  preterm * proof" neede?         *)
+(* TODO: I the first argument of "Pimp_r of  preterm * proof" needed?        *)
 (* ------------------------------------------------------------------------- *)
 
 type proof =
@@ -59,61 +59,88 @@ let rec pcheck : proof -> tactic =
   | Pforall_r (x,_,p) ->
       let tmx = term_of_preterm x in
       X_GEN_TAC tmx THEN pcheck p
-
-(*
-   | Pexists_l of  preterm * preterm * proof
-   | Pexists_r of  preterm * proof;;
-*)
-
+  | Pexists_l(x,a,p) ->
+      let tmx = term_of_preterm x
+      and tma = term_of_preterm a in
+      let th = ASSUME (mk_exists(tmx,tma)) in
+      X_CHOOSE_TAC tmx th THEN pcheck p
+  | Pexists_r(a,p) ->
+       let tmx = term_of_preterm a in
+       EXISTS_TAC tmx THEN pcheck p
   | Pall -> ALL_TAC
   | _ -> failwith "Not implemented yet"
 ;;
 
+(* ------------------------------------------------------------------------ *)
+(* Tests                                                                    *)
+(* ------------------------------------------------------------------------ *)
 
-(* Test 1 *)
 let ptma = preterm_of_term `a:bool`
 and ptmb = preterm_of_term `b:bool`;;
-let prf1 = Pand_l(ptma,ptmb,Pinitial ptma);;
 
+(* Test 1 *)
 g `a /\ b ==> a`;;
 e DISCH_TAC;;
+let prf1 = Pand_l(ptma,ptmb,Pinitial ptma);;
 e (pcheck prf1);;
+top_thm();;
 
 (* Test 2 *)
-let prf2 = Pimp_r(ptma,prf1);;
 g `a /\ b ==> a`;;
+let prf2 = Pimp_r(ptma,prf1);;
 e (pcheck prf2);;
+top_thm();;
 
 (* Test 3 *)
 g `a ==> a \/ b`;;
 let prf = Pimp_r(ptma,(Por1_r(ptma,Pinitial ptma)));;
 e (pcheck prf);;
+top_thm();;
 
 (* Test 4 *)
 g `b ==> a \/ b`;;
 let prf = Pimp_r(ptma,(Por2_r(ptma,Pinitial ptmb)));;
 e (pcheck prf);;
+top_thm();;
 
 (* Test 5 *)
 g `a \/ a ==> a`;;
 let prf = Pimp_r(ptma,Por_l(ptma,ptma,Pinitial ptma,Pinitial ptma));;
 e (pcheck prf);;
+top_thm();;
 
 (* Test 6 *)
 g `F ==> a`;;
 let prf = Pimp_r(ptma,Pex_falso);;
 e (pcheck prf);;
+top_thm();;
 
 (* Test 6 *)
 let ptmab = preterm_of_term `a ==> b`;;
 g `a /\ (a ==> b) ==> b`;;
 let prf = Pimp_r(ptma,Pand_l(ptma,ptmab,Pimp_l(ptma,ptmb,Pinitial ptma,Pinitial ptmb)));;
 e (pcheck prf);;
+top_thm();;
 
 (* Test 7 *)
 g `!a. F ==> a`;;
 let prf = Pforall_r(ptma,ptma,Pimp_r(ptma,Pex_falso));;
 e (pcheck prf);;
+top_thm();;
+
+(* Test 8 *)
+g `(?u:A. F) ==> a`;;
+let ptmu = preterm_of_term `u:A`;;
+let ptmff = preterm_of_term `F`;;
+let prf = Pimp_r(ptma,Pexists_l(ptmu,ptmff,Pex_falso));;
+e (pcheck prf);;
+top_thm();;
+
+(* Test 9 *)
+g `?a:bool. a ==> b`;;
+let prf = Pexists_r(ptmff,Pimp_r(ptma,Pex_falso));;
+e (pcheck prf);;
+top_thm();;
 
 
 (*

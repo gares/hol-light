e693d71a12092839b5fdd760840a94f9af6d0603
Marco Maggesi <1809783+maggesi@users.noreply.github.com>
2019-09-11 13:48:14 +0200

    Now infer works on all terms extracted from HOL traces.

---
+++ elpi/run_no_elpi.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+elpi no_hol.elpi -test
--- elpi/test_parsed_preterms.ml
+++ elpi/test_parsed_preterms.ml
@@ -10,17 +10,6 @@ let term_eq tm1 tm2 =
       forall (is_vartype o fst) tinst
   with Failure _ -> false;;
 
-(* Variant of the above. *)
-(*
-let rec term_eq tm1 tm2 =
-  match tm1,tm2 with
-    Var(s1,_), Var(s2,_)
-  | Const(s1,_), Const(s2,_) -> s1 = s2
-  | Comb(f1,x1), Comb(f2,x2) -> term_eq f1 f2 && term_eq x1 x2
-  | Abs(Var(s1,_),b1), Abs(Var(s2,_),b2) -> s1 = s2 && term_eq b1 b2
-  | _ -> false;;
-*)
-
 (* ------------------------------------------------------------------------- *)
 (* Setup.                                                                    *)
 (* ------------------------------------------------------------------------- *)
@@ -31,6 +20,11 @@ unreserve_words ["^"];; (* "^" is used for antiquotation *)
 (* Load the program *)
 let prg = ref (Hol_elpi.hol());;
 
+(* Run the query. *)
+let elaborate (ptm : preterm) : term =
+  let ptm = Hol_elpi.elaborate_preterm_with !prg ptm in
+  term_of_preterm ptm;;
+
 (* Use one of the following. *)
 let pterms = load_parsed_terms "elpi/CORE.bin";;
 let pterms = load_parsed_terms "elpi/MULTIVARIATE.bin";;
@@ -40,61 +34,12 @@ let pterms = load_parsed_terms "elpi/HYPERCOMPLEX.bin";;
 (* Number of terms. *)
 length pterms;;
 
-(* ------------------------------------------------------------------------- *)
-(* Example tests.                                                            *)
-(* ------------------------------------------------------------------------- *)
-
-(* Returns true only if Noparse is raised. *)
-let term_noparse (s,ptm,tm,st) =
-  let p = parse_preterm o lex o explode in
-  try set_hol_status st;
-      ignore(p s);
-      false
-  with Failure _ -> false
-     | Noparse -> true;;
-
-let ko_terms = filter_progress term_noparse pterms;;
-length ko_terms;;
-
-let elaborate (ptm : preterm) : term =
-  let ptm = Hol_elpi.elaborate_preterm_with !prg ptm in
-  term_of_preterm ptm;;
-
-(* Returns true if the elaborator fails (including GABS). *)
-let term_noelab (_,ptm,tm,st) =
-  set_hol_status st;
-  not (can elaborate ptm);;
-
-(* Same as above, excluding GABS. *)
-let term_noelab (_,ptm,tm,st) =
-  if contains_gabs tm then false else
-  begin
-    set_hol_status st;
-    not (can elaborate ptm)
-  end;;
-
-let ko_terms = filter_progress term_noelab pterms;;
-length ko_terms;;
-(* Run 2019-08-30: All terms in CORE pass this test with elaborate. *)
-(* Run 2019-08-22: All terms without GABS in CORE pass this test with elab. *)
-
-(* Returns true if the elaborator returns a different term. *)
-(* NB2: (Also return true if the elaborator fails.) *)
+(* Returns true if the elaborator fails or returns a different term. *)
 let term_elab_neq (_,ptm,tm,st) =
   set_hol_status st;
   try let qtm = elaborate ptm in
       not (term_eq tm qtm)
   with Failure _ -> true;;
-
-(* Same as above, excluding GABS. *)
-let term_elab_neq (_,ptm,tm,st) =
-  if contains_gabs tm then false else
-  begin
-    set_hol_status st;
-    try let qtm = Hol_elpi.elaborate ptm in
-        not (term_eq tm qtm)
-    with Failure _ -> true
-  end;;
 
 let ko_terms = filter_progress term_elab_neq pterms;;
 length ko_terms;;
@@ -110,25 +55,20 @@ prg := Hol_elpi.hol();;
 Hol_elpi.elab_predicate := "elab";;
 Hol_elpi.elab_predicate := "elaborate";;
 
-length pterms;;
-
-length ko_terms;;
-
-let ko_terms = filter_progress term_noelab (take 200 pterms);;
-
 map (fun str,ptm,tm,st -> tm) ko_terms;;
 
 let ko_terms2 = filter_progress term_noelab ko_terms;;
 length ko_terms2;;
 
-let str,ptm,tm,st = hd (ko_terms);;
+let str,ptm,tm,st = hd ko_terms;;
+tm;;
+report str;;
 set_hol_status st;;
-let ptm' = Hol_elpi.elaborate_preterm ptm;;
+let ptm' = Hol_elpi.elaborate_preterm_with !prg ptm;;
 let tm' = term_of_preterm ptm';;
+term_eq tm tm';;
 
 
-Hol_elpi.elaborate_preterm ptm;;
-(Hol_elpi.elaborate_preterm o fst o parse_preterm o lex o explode) "coprime";;
 
 filter_progress
     (fun (_,_,tm,_,_,_) ->
@@ -143,4 +83,11 @@ let parsing_fail s =
 let ptm = parse_term "{x | P x}";;
 let qtm = Hol_elpi.quotation "{x | P x}";;
 
-term_eq ptm qtm;;
+let ptm1 = (fst o parse_preterm o lex o explode) "IN m {m:num | m | < m n}";;
+(* let ptm1 = (fst o parse_preterm o lex o explode) "{m:num | m | < m n}";; *)
+let tm1 = term_of_preterm (retypecheck [] ptm1);;
+(map dest_var o variables) tm1;;
+let ptm1' = Hol_elpi.elaborate_preterm_with !prg ptm1;;
+let tm1' = term_of_preterm ptm1';;
+(map dest_var o variables) tm1';;
+term_eq tm1 tm1';;
